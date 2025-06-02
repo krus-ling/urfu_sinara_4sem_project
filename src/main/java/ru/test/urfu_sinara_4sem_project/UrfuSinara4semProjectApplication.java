5a26dfcbcc0a918f4ffb9b05ed283bfdc5c62842
krus-ling <egorguk2005@outlook.com>
2025-06-02 17:56:37 +0500

    init

---
+++ src/main/java/ru/test/urfu_sinara_4sem_project/UrfuSinara4semProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.test.urfu_sinara_4sem_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrfuSinara4semProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrfuSinara4semProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/test/urfu_sinara_4sem_project/UrfuSinara4semProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.test.urfu_sinara_4sem_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrfuSinara4semProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
